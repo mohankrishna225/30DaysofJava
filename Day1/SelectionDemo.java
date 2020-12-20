f3da87794b81206b016dddd7b0f9b30c3cd1b351
mohankrishna225 <www.krishna225@gmail.com>
2020-12-20 21:03:51 +0530

    Create SelectionDemo.java

---
+++ Day1/SelectionDemo.java
@@ -0,0 +1,18 @@
+public class SelectionDemo
+{
+	public static void main(String[] args) {
+		int m=0;
+		if(m==0)
+		{
+			System.out.println("Neither Even Nor Odd");
+		}
+		else if(m%2==0)
+		{
+			System.out.println("Even");
+		}
+		else
+		{
+			System.out.println("Odd");
+		}
+	}
+}
