a547113223820e3a3f868292e02e9d26c6731536
mohankrishna225 <www.krishna225@gmail.com>
2020-12-21 01:40:57 +0530

    Create InnerDemo.java

---
+++ Day2/InnerDemo.java
@@ -0,0 +1,46 @@
+class Outer
+{
+	int a;
+	public void show()
+	{
+		System.out.println("Inside Outer");
+	}
+
+
+	class Inner1
+	{
+		public void display()
+		{
+			System.out.println("In display from Inner1");
+		}
+	}
+
+	//We can Make variables, methods and classes static as well;
+
+	static class Inner
+	{
+		public void display()
+		{
+			System.out.println("In display from Inner");
+		}
+	}
+
+}
+
+public class InnerDemo
+{
+	public static void main(String[] args) {
+		
+		Outer obj = new Outer();
+		obj.show();
+
+
+		//Object Creation for Inner Classes
+		Outer.Inner1 obj1 = obj.new Inner1();
+		obj1.display();
+
+		//object creation for Static Inner Class;
+		Outer.Inner obj2 = new Outer.Inner();
+		obj2.display();
+	}
+}
