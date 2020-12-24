edbcfc2d12a9f3a3128e5f66d57ed6665d2e66f6
mohankrishna225 <www.krishna225@gmail.com>
2020-12-25 03:42:31 +0530

    Update RegexDemo.java

--- Day5/RegexDemo.java
+++ Day5/RegexDemo.java
@@ -1,5 +1,22 @@
 import java.util.regex.*;
 
+// -> \d for digits
+// -> \S for whitespaces
+// -> \w A word Character(letter|digits|_underscore);
+//TO specify a set of Characters to search for 
+// [abc] search for a's, b's, c's;
+// [a-f] searches only for a,b,c,d,e,f characters;
+// [a-f A-F] -search for first six letters
+//----- Quantifiers --------
+// 0x{0-1 or a-f or A-F} for Hexadecimals --> Generally 0[xX][0-9a-fA-F] (find one or more two digits in a row)
+// source: "1 a12 234b 45a" 
+// + -> one or more occurences
+// * -> Zero or more occurences
+// ? -> Zero or one Occurence
+// So now Hexademical is 0[xX]([0-9a-fA-F])+
+// Predefined dot "." -> means any character can served here 
+
+
 class RegexDemo
 {
 	public static void main(String[] args) {
